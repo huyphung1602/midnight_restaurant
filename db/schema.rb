4f451ea7846e57e5987b7fbec875b253fd8aae22
huyphung1602 <huy.phung.sw@gmail.com>
2017-02-15 16:07:14 +0700

    generate orders resources of food_items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214142738) do
+ActiveRecord::Schema.define(version: 20170215090305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20170214142738) do
     t.index ["section_id"], name: "index_food_items_on_section_id", using: :btree
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.integer  "food_item_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["food_item_id"], name: "index_orders_on_food_item_id", using: :btree
+  end
+
   create_table "sections", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -34,4 +41,5 @@ ActiveRecord::Schema.define(version: 20170214142738) do
   end
 
   add_foreign_key "food_items", "sections"
+  add_foreign_key "orders", "food_items"
 end
